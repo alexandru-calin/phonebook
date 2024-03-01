a335e9723aeed4f3427a694e5322b2e157f64192
alexandru calin <alexandrucalin@proton.me>
2024-03-01 19:56:58 +0200

    added POST route to create a new phonebook entry

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     "id": 1,
@@ -53,6 +55,16 @@ app.delete("/api/persons/:id", (req, res) => {
   persons = persons.filter(p => p.id !== id);
 
   res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+
+  person.id = Math.floor(Math.random() * 10000);
+
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
 const PORT = 3001;
