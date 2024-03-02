83fe6ec6dec8b63d2751366e5fd6875b6844cffe
alexandru calin <alexandrucalin@proton.me>
2024-03-02 15:04:28 +0200

    added new morgan token for logging request body

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 app.use(express.json());
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 let persons = [
   {
